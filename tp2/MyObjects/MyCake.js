b718dc9f768a6e2dc4f65b1c79fb8d1b6ad77f0d
Maria Sofia Gonçalves <80844884+MSofiaGoncalves@users.noreply.github.com>
2023-09-29 12:12:20 +0100

    atualized cake and contents

--- tp2/MyObjects/MyCake.js
+++ tp2/MyObjects/MyCake.js
@@ -20,6 +20,13 @@ class MyCake extends THREE.Object3D {
         this.down_radius = 0.25;
         this.flavor = flavor;
 
+        this.planeCakeMaterial = new THREE.MeshPhongMaterial({
+            color: "#FFCC80",
+            specular: "#6E260E",
+            emissive: "#000000",
+            shininess: 10,
+        });
+
         if(this.flavor === "strawberry"){
             this.cakeMaterial = new THREE.MeshPhongMaterial({
                 color: "#FEC5E5",
@@ -58,18 +65,46 @@ class MyCake extends THREE.Object3D {
             0,
             (14*Math.PI)/8
         );
-        this.cakeMesh = new THREE.Mesh(cake, this.cakeMaterial);
+
+        this.cakeGroup = new THREE.Group();
+
+        ["#805A46", "#FFF6DF", "#FEC5E5"].forEach((color, index) => {
+            let cakeMaterial = new THREE.MeshPhongMaterial({
+                color,
+                specular: "#6E260E",
+                emissive: "#000000",
+                shininess: 10,
+            });
+
+            this.cakeMesh = new THREE.Mesh(cake, cakeMaterial);
+        
+            this.cakeMesh.position.y = 0.24 + 0.1*index;
 
 
+            this.cakeGroup.add(this.cakeMesh)
 
-        this.cakeMesh.position.y = 0.24;
-        //this.flavor = "strawberry";
+        })
 
 
+        let planeCake = new THREE.PlaneGeometry( 
+            0.22, 
+            0.3 
+        );
+        
+        this.planeLeftCakeMesh = new THREE.Mesh( planeCake, this.planeCakeMaterial);
+        this.planeLeftCakeMesh.position.y = 0.34;
+        this.planeLeftCakeMesh.position.x = -0.1;
+        this.planeLeftCakeMesh.position.z = 0.1
+        this.planeLeftCakeMesh.rotation.y =  Math.PI / 4;
+        
 
-        this.cakeGroup = new THREE.Group()
+        this.planeRightCakeMesh = new THREE.Mesh( planeCake, this.planeCakeMaterial);
+        this.planeRightCakeMesh.position.y = 0.34;
+        this.planeRightCakeMesh.position.z = 0.14;
+        this.planeRightCakeMesh.rotation.y =  -2 * Math.PI / 4;
 
-        this.cakeGroup.add(this.cakeMesh)
+        this.cakeGroup.add(this.planeLeftCakeMesh);
+        this.cakeGroup.add(this.planeRightCakeMesh);
 
 
         this.add(this.cakeGroup);
