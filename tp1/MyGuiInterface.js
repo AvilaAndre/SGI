c84773e40fe879bc50d4d51250346d5be987a9b4
AvilaAndre <avila_andre14@hotmail.com>
2023-10-18 18:02:28 +0100

    removed not working GUI components

--- tp1/MyGuiInterface.js
+++ tp1/MyGuiInterface.js
@@ -28,42 +28,6 @@ class MyGuiInterface {
      * Initialize the gui interface
      */
     init() {
-        // add a folder to the gui interface for the box
-        const boxFolder = this.datgui.addFolder("Box");
-        // note that we are using a property from the contents object
-        boxFolder
-            .add(this.contents, "boxMeshSize", 0, 10)
-            .name("size")
-            .onChange(() => {
-                this.contents.rebuildBox();
-            });
-        boxFolder.add(this.contents, "boxEnabled", true).name("enabled");
-        boxFolder.add(this.contents.boxDisplacement, "x", -5, 5);
-        boxFolder.add(this.contents.boxDisplacement, "y", -5, 5);
-        boxFolder.add(this.contents.boxDisplacement, "z", -5, 5);
-        boxFolder.open();
-
-        const data = {
-            "diffuse color": this.contents.diffusePlaneColor,
-            "specular color": this.contents.specularPlaneColor,
-        };
-
-        // adds a folder to the gui interface for the plane
-        const planeFolder = this.datgui.addFolder("Plane");
-        planeFolder.addColor(data, "diffuse color").onChange((value) => {
-            this.contents.updateDiffusePlaneColor(value);
-        });
-        planeFolder.addColor(data, "specular color").onChange((value) => {
-            this.contents.updateSpecularPlaneColor(value);
-        });
-        planeFolder
-            .add(this.contents, "planeShininess", 0, 1000)
-            .name("shininess")
-            .onChange((value) => {
-                this.contents.updatePlaneShininess(value);
-            });
-        planeFolder.open();
-
         const cameras = [
             "Perspective",
             "Left",
@@ -164,21 +128,21 @@ class MyGuiInterface {
             })
             .name("Move Curtains");
 
-        const wallLampsFolder = lightsFolder.addFolder("Wall Lamps");
+        // const wallLampsFolder = lightsFolder.addFolder("Wall Lamps");
 
-        wallLampsFolder
-            .addColor(lightsData, "wallLampsColor")
-            .onChange((value) => {
-                this.contents.updateWallLampsColor(value);
-            })
-            .name("Wall Lamps Color");
+        // wallLampsFolder
+        //     .addColor(lightsData, "wallLampsColor")
+        //     .onChange((value) => {
+        //         this.contents.updateWallLampsColor(value);
+        //     })
+        //     .name("Wall Lamps Color");
 
-        wallLampsFolder
-            .add(lightsData, "wallLampsIntensity", 0, 20)
-            .onChange((value) => {
-                this.contents.updateWallLampsIntensity(value);
-            })
-            .name("Wall Lamps Intensity");
+        // wallLampsFolder
+        //     .add(lightsData, "wallLampsIntensity", 0, 20)
+        //     .onChange((value) => {
+        //         this.contents.updateWallLampsIntensity(value);
+        //     })
+        //     .name("Wall Lamps Intensity");
     }
 }
 
