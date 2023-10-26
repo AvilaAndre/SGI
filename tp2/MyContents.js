994de39bf6beac722333e9db797e1ae3b1876adc
AvilaAndre <avila_andre14@hotmail.com>
2023-10-26 23:14:38 +0100

    Add texture

--- tp2/MyContents.js
+++ tp2/MyContents.js
@@ -1,43 +1,39 @@
-import * as THREE from 'three';
-import { MyAxis } from './MyAxis.js';
-import { MyFileReader } from './parser/MyFileReader.js';
+import * as THREE from "three";
+import { MyAxis } from "./MyAxis.js";
+import { MyFileReader } from "./parser/MyFileReader.js";
 /**
  *  This class contains the contents of out application
  */
-class MyContents  {
-
+class MyContents {
     /**
        constructs the object
        @param {MyApp} app The application object
-    */ 
+    */
     constructor(app) {
-        this.app = app
-        this.axis = null
+        this.app = app;
+        this.axis = null;
         // global settings
         this.background = null;
         this.ambient = null;
+        // textures
+        this.textures = new Object();
         // materials
         this.materials = [];
 
-
         this.reader = new MyFileReader(app, this, this.onSceneLoaded);
-		this.reader.open("scenes/demo/demo.xml");		
+        this.reader.open("scenes/demo/demo.xml");
     }
 
     /**
      * initializes the contents
      */
     init() {
-        // create once 
+        // create once
         if (this.axis === null) {
             // create and attach the axis to the scene
-            this.axis = new MyAxis(this)
-            this.app.scene.add(this.axis)
+            this.axis = new MyAxis(this);
+            this.app.scene.add(this.axis);
         }
-
-        // TODO: Init Background
-
-        // TODO: Init Ambient
     }
 
     /**
@@ -45,64 +41,87 @@ class MyContents  {
      * @param {MySceneData} data the entire scene data object
      */
     onSceneLoaded(data) {
-        console.info("scene data loaded " + data + ". visit MySceneData javascript class to check contents for each data item.")
+        console.info(
+            "scene data loaded " +
+                data +
+                ". visit MySceneData javascript class to check contents for each data item."
+        );
         this.onAfterSceneLoadedAndBeforeRender(data);
     }
 
     output(obj, indent = 0) {
-        console.log("" + new Array(indent * 4).join(' ') + " - " + obj.type + " " + (obj.id !== undefined ? "'" + obj.id + "'" : ""))
+        console.log(
+            "" +
+                new Array(indent * 4).join(" ") +
+                " - " +
+                obj.type +
+                " " +
+                (obj.id !== undefined ? "'" + obj.id + "'" : "")
+        );
     }
 
     onAfterSceneLoadedAndBeforeRender(data) {
-       
         // refer to descriptors in class MySceneData.js
         // to see the data structure for each item
 
-        // this.output(data.options)
+        this.output(data.options);
 
-        this.setOptions(data.options)
+        this.setOptions(data.options);
 
-        console.log("textures:")
+        console.log("textures:");
         for (var key in data.textures) {
-            let texture = data.textures[key]
-            this.output(texture, 1)
-            this.addTexture(texture)
+            let texture = data.textures[key];
+            this.output(texture, 1);
+            this.addTexture(texture);
         }
 
-        console.log("materials:")
+        console.log("materials:");
         for (var key in data.materials) {
-            let material = data.materials[key]
-            this.output(material, 1)
+            let material = data.materials[key];
+            this.output(material, 1);
         }
 
-        console.log("cameras:")
+        console.log("cameras:");
         for (var key in data.cameras) {
-            let camera = data.cameras[key]
-            this.output(camera, 1)
+            let camera = data.cameras[key];
+            this.output(camera, 1);
         }
 
-        console.log("nodes:")
+        console.log("nodes:");
         for (var key in data.nodes) {
-            let node = data.nodes[key]
-            this.output(node, 1)
-            for (let i=0; i< node.children.length; i++) {
-                let child = node.children[i]
+            let node = data.nodes[key];
+            this.output(node, 1);
+            for (let i = 0; i < node.children.length; i++) {
+                let child = node.children[i];
                 if (child.type === "primitive") {
-                    console.log("" + new Array(2 * 4).join(' ') + " - " + child.type + " with "  + child.representations.length + " " + child.subtype + " representation(s)")
+                    console.log(
+                        "" +
+                            new Array(2 * 4).join(" ") +
+                            " - " +
+                            child.type +
+                            " with " +
+                            child.representations.length +
+                            " " +
+                            child.subtype +
+                            " representation(s)"
+                    );
                     if (child.subtype === "nurbs") {
-                        console.log("" + new Array(3 * 4).join(' ') + " - " + child.representations[0].controlpoints.length + " control points")
+                        console.log(
+                            "" +
+                                new Array(3 * 4).join(" ") +
+                                " - " +
+                                child.representations[0].controlpoints.length +
+                                " control points"
+                        );
                     }
-                }
-                else {
-                    this.output(child, 2)
+                } else {
+                    this.output(child, 2);
                 }
             }
         }
     }
 
-    update() {
-        
-    }
+    update() {}
 
     setOptions(options) {
         this.background = JSON.stringify(options.background) || 0;
@@ -110,8 +129,21 @@ class MyContents  {
     }
 
     addTexture(texture) {
-        let textureObj = JSON.parse(JSON.stringify(texture))
-        console.log('TEXTURE' + JSON.parse(JSON.stringify(texture)).id) 
+        const newTexture = new THREE.Texture(
+            texture.filepath,
+            undefined,
+            undefined,
+            undefined,
+            texture.magFilter == "LinearFilter"
+                ? THREE.LinearFilter
+                : THREE.NearestFilter,
+            texture.minFilter, // This is a string instead of the actual value
+            undefined,
+            undefined,
+            texture.anisotropy
+        );
+
+        this.textures[texture.id] = newTexture;
     }
 }
 
