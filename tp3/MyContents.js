70ea49fe1f1c2f4bac3788abe7ddbc53f9ffe13d
Maria Sofia Gonçalves <80844884+MSofiaGoncalves@users.noreply.github.com>
2023-12-20 12:44:46 +0000

    obstacle park done

--- tp3/MyContents.js
+++ tp3/MyContents.js
@@ -56,7 +56,7 @@ class MyContents {
         this.scenePath = "scenes/scene1/";
 
         this.reader = new MyFileReader(app, this, this.onSceneLoaded);
-        this.reader.open(this.scenePath + "OPPONENTPark.xml");
+        this.reader.open(this.scenePath + "obstaclePark.xml");
     }
 
     /**
